c5ba4af2093b7b951200dda0b34dd4b8c1ce43c4
Alex Wendland <a.p.wendland@gmail.com>
2024-03-30 10:36:08 +0000

    Add precommit

--- tests/game_engine/test_board.py
+++ tests/game_engine/test_board.py
@@ -14,4 +14,23 @@ def test_initilisation(size: int):
     assert board._state[0][0] == Player.NO_PLAYER
 
 
-# @pytest.fixture
+@pytest.fixture
+def no_geometry_board():
+    board = NoGeometryBoard(size=5)
+    board.set_position(0, 0, Player.ONE)
+    board.set_position(0, 1, Player.TWO)
+    board.set_position(2, 0, Player.ONE)
+    return board
+
+
+@pytest.mark.parametrize(
+    ["column", "row", "player"],
+    [
+        (0, 0, Player.ONE),
+        (0, 1, Player.TWO),
+        (2, 2, Player.NO_PLAYER),
+        (-1, -1, NOT_A_POSITION),
+    ],
+)
+def test_get_position(no_geometry_board, column, row, player):
+    assert no_geometry_board.get_position(column, row) == player
